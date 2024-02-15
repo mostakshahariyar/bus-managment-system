d9f47c9a3181c28dc8253c6d2bcfa6db18c07338
mostakshahariyar <mostakshahariyar18@gmail.com>
2024-02-16 01:42:13 +0600

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <h1 className='text-4xl font-4xl text-yellow-500 text-center'>this is test </h1>
+   </>
+  )
+}
+
+export default App
